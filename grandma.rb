7b83a46bfe6a89c65e2acad6b3fcc8b70579d7c1
Hoon Lee <yl.hoony@gmail.com>
2017-10-30 08:04:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
   puts "HUH?! SPEAK UP, SONNY!"
 end
