f1b72a32072e4cff49a5bbfed756bf4688302f97
Hoon Lee <yl.hoony@gmail.com>
2017-10-30 08:11:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?" || "Hi!"
+  elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
